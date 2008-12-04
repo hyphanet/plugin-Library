78d1419f26cb490e0b34b0c1324229a0293d004a
Daniel Cheng (鄭郁邦) <j16sdiz+freenet@gmail.com>
2008-12-04 09:08:34 +0000

    XMLLibrarian part of the patch (bug 1714) Indexer now works, Librarian not tested
    git-svn-id: file:///home/toad/git-migration/temprepository/trunk/plugins/XMLLibrarian@24034 67a373e5-eb02-0410-a15c-ee090a768436

--- XMLLibrarian.java
+++ XMLLibrarian.java
@@ -940,7 +940,7 @@ public class XMLLibrarian implements FredPlugin, FredPluginHTTP, FredPluginThrea
 		MessageDigest md;
 		md = MessageDigest.getInstance("MD5");
 		byte[] md5hash = new byte[32];
-		md.update(text.getBytes("iso-8859-1"), 0, text.length());
+		md.update(text.getBytes("UTF-8"), 0, text.length());
 		md5hash = md.digest();
 		return convertToHex(md5hash);
 	}
