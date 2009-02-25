b9c3d87523a163293acb4b18fce91b7c9e7c78f4
Daniel Cheng (鄭郁邦) <j16sdiz+freenet@gmail.com>
2009-02-25 06:58:06 +0000

    Set SAXParser load-external-dtd to false
    git-svn-id: file:///home/toad/git-migration/temprepository/trunk/plugins/XMLLibrarian@25811 67a373e5-eb02-0410-a15c-ee090a768436

--- XMLLibrarian.java
+++ XMLLibrarian.java
@@ -326,6 +326,7 @@ public class XMLLibrarian implements FredPlugin, FredPluginHTTP, FredPluginVersi
 
 		SAXParserFactory factory = SAXParserFactory.newInstance();
 		try {
+			factory.setFeature("http://apache.org/xml/features/nonvalidating/load-external-dtd", false);
 			SAXParser saxParser = factory.newSAXParser();
 			InputStream is = bucket.getInputStream();
 			LibrarianHandler lib = new LibrarianHandler(str, new Vector<URIWrapper>());
@@ -358,6 +359,7 @@ public class XMLLibrarian implements FredPlugin, FredPluginHTTP, FredPluginVersi
 
 			SAXParserFactory factory = SAXParserFactory.newInstance();
 			try {
+				factory.setFeature("http://apache.org/xml/features/nonvalidating/load-external-dtd", false);
 				SAXParser saxParser = factory.newSAXParser();
 				InputStream is = bucket.getInputStream();
 				saxParser.parse(is, new LibrarianHandler(str, fileuris));
