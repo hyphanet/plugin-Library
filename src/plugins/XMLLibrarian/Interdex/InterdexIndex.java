56b805af2ccd14c0a11c20bc30e79dab8d7d297b
platy <mpbush@gmail.com>
2009-06-24 20:14:03 +0100

    Implemented possible Request & Index classes for Interdex attachmentjavac

---
+++ src/plugins/XMLLibrarian/Interdex/InterdexIndex.java
@@ -0,0 +1,18 @@
+package plugins.XMLLibrarian.Interdex;
+
+import plugins.XMLLibrarian.Request;
+import plugins.Interdex.index.Index;
+
+public class InterdexIndex extends plugins.XMLLibrarian.Index {
+	Index index;
+
+	public InterdexIndex(Index index){
+		this.index = index;
+	}
+
+	@Override
+	public Request find(String term) {
+		throw new UnsupportedOperationException();
+	}
+
+}
