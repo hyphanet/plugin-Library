848d26a6a52abe3592ca71f5a665bb5c83ae8c79
platy <mpbush@gmail.com>
2009-07-06 17:09:33 +0100

    Remove unused parts of LibrarianHandler XML parsing

--- src/plugins/XMLLibrarian/xmlindex/LibrarianHandler.java
+++ src/plugins/XMLLibrarian/xmlindex/LibrarianHandler.java
@@ -21,21 +21,12 @@ import plugins.XMLLibrarian.URIWrapper;
  * 
  */
 public class LibrarianHandler extends DefaultHandler {
-	private final String word;
-	private final String md5;
-
-	private int prefix;
 	private boolean processingWord;
-	private String prefixMatch;
 
-	// now we need to adapt this to read subindexing 
-	private boolean found_match;
 	/** file id -> uri */
 	private HashMap<String, String> uris;
 	/** file id -> title */
 	private HashMap<String, String> titles;
-	private List<URIWrapper> fileuris;
-	///private List<Integer> fileids;
 	private List<FindRequest> requests;
 	private List<FindRequest> wordMatches;
 
@@ -49,8 +40,6 @@ public class LibrarianHandler extends DefaultHandler {
 		this.requests = requests;
 		for(FindRequest r : requests)
 			r.setResult(new HashSet<URIWrapper>());
-		word = "";
-		md5="";
 	}
 
 	public void setDocumentLocator(Locator value) {
@@ -61,7 +50,6 @@ public class LibrarianHandler extends DefaultHandler {
 	}
 
 	public void startDocument() throws SAXException {
-		found_match = false;
 		if(uris==null || titles ==null){
 			uris = new HashMap<String, String>();
 			titles = new HashMap<String, String>();
@@ -74,12 +62,6 @@ public class LibrarianHandler extends DefaultHandler {
 			rawName = localName;
 		}
 		String elt_name = rawName;
-		/*
-		 * Gives the maximum number of digits of md5 used for creating subindices
-		 */
-		if (elt_name.equals("prefix")) {
-			prefix = Integer.parseInt(attrs.getValue("value"));
-		}
 
 		if (elt_name.equals("files"))
 			processingWord = false;
@@ -91,11 +73,8 @@ public class LibrarianHandler extends DefaultHandler {
 		 */
 		if (elt_name.equals("word")) {
 			try {
-				found_match = false;
 				wordMatches = null;
 				String match = attrs.getValue("v");
-				if (match.equals(word))
-					found_match = true;
 				if (requests!=null){
 					wordMatches = new ArrayList<FindRequest>();
 					for (FindRequest r : requests){
@@ -111,39 +90,21 @@ public class LibrarianHandler extends DefaultHandler {
 		}
 
 		if (elt_name.equals("file")) {
-			if (processingWord == true && (found_match == true || wordMatches!=null)) {
+			if (processingWord == true &&  wordMatches!=null) {
 				try{
-					if(fileuris!=null){
-						URIWrapper uri = new URIWrapper();
-						uri.URI = uris.get(attrs.getValue("id"));
-						synchronized(this){
-							if(titles.containsKey(attrs.getValue("id")))
-							{
-								uri.descr = titles.get(attrs.getValue("id"));
-								if ((uri.URI).equals(uri.descr))
-									uri.descr = "not available";
-							}
-							else
+					URIWrapper uri = new URIWrapper();
+					uri.URI = uris.get(attrs.getValue("id"));
+					synchronized(this){
+						if(titles.containsKey(attrs.getValue("id")))
+						{
+							uri.descr = titles.get(attrs.getValue("id"));
+							if ((uri.URI).equals(uri.descr))
 								uri.descr = "not available";
-
-							if (fileuris != null)
-								fileuris.add(uri);
 						}
-					}else{
-						URIWrapper uri = new URIWrapper();
-						uri.URI = uris.get(attrs.getValue("id"));
-						synchronized(this){
-							if(titles.containsKey(attrs.getValue("id")))
-							{
-								uri.descr = titles.get(attrs.getValue("id"));
-								if ((uri.URI).equals(uri.descr))
-									uri.descr = "not available";
-							}
-							else
-								uri.descr = "not available";
-							for(FindRequest<URIWrapper> match : wordMatches){
-								match.addResult(uri);
-							}
+						else
+							uri.descr = "not available";
+						for(FindRequest<URIWrapper> match : wordMatches){
+							match.addResult(uri);
 						}
 					}
 				}
@@ -166,7 +127,6 @@ public class LibrarianHandler extends DefaultHandler {
 								Logger.error(this, "Index Format not compatible " + e.toString(), e);
 							}
 						}
-
 						uris.put(id, key);
 					}
 				}
@@ -176,9 +136,4 @@ public class LibrarianHandler extends DefaultHandler {
 			}
 		}
 	}
-
-	public String getPrefixMatch() {
-		return prefixMatch;
-	}
-
 }
