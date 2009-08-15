hyphanet/plugin-Library

commit 40763ffe9e5dc295210aeb9595dee648db002c07

Asynchronously perform search logic operations on search results and generate result HTMLNode

src/plugins/Library/search/ResultSet.java
@@ -23,11 +23,15 @@ import plugins.Library.serial.TaskAbortException;
  * Unmodifiable Set which makes sure all data in results being combined is
  * retained and is created as the result of Set operations on other Sets. Does
  * not contain references to the original Sets.
+ * <br /> <br />
+ * The set isn't finalised until a run is finished and isDone() is true.
  *
- * FIXME get phrase search to work
  * @author MikeB
  */
-public class ResultSet implements Set<TermEntry>{
+public class ResultSet implements Set<TermEntry>, Runnable{
+	private ResultOperation resultOperation;
+	private boolean done = false;
+	private Set<TermEntry>[] subresults;
 
 	/**
 	 * What should be done with the results of subsearches\n
@@ -62,26 +66,35 @@ public class ResultSet implements Set<TermEntry>{
 				&& subRequests.size()<2)
 			throw new IllegalArgumentException(resultOperation.toString() + " operations need more than one term");
 
+		// Make sure any TaskAbortExceptions are found here and not when it's run
+		subresults = getResultSets(subRequests);
+
 		this.subject = subject;
 		internal = new HashMap();
+		this.resultOperation = resultOperation;
+	}
 
+	public synchronized void run() {
+		if(done)
+			throw new IllegalStateException("This ResultSet has already run and is finalised.");
+		
 		// Decide what to do
 		switch(resultOperation){
 			case SINGLE:	// Just add everything
-				addAllToEmptyInternal(subRequests.get(0).getResult());
+				addAllToEmptyInternal(subresults[0]);
 				break;
 			case DIFFERENTINDEXES:	// Same as UNION currently
 			case UNION:	// Add every one without overwriting
-				unite(getResultSets(subRequests));
+				unite(subresults);
 				break;
 			case INTERSECTION:	// Add one then retain the others
-				intersect(getResultSets(subRequests));
+				intersect(subresults);
 				break;
 			case REMOVE:	// Add one then remove the other
-				exclude(subRequests.get(0).getResult(), subRequests.get(1).getResult());
+				exclude(subresults[0], subresults[1]);
 				break;
 			case PHRASE:
-				phrase(getResultSets(subRequests));
+				phrase(subresults);
 				break;
 		}
 	}
@@ -463,6 +476,10 @@ public class ResultSet implements Set<TermEntry>{
 
 	public String toString(){
 		return internal.keySet().toString();
+	}
+
+	public boolean isDone(){
+		return done;
 	}
 
 

src/plugins/Library/Main.java
@@ -55,7 +55,7 @@ public class Main implements FredPlugin, FredPluginVersioned, freenet.pluginmana
 	public void runPlugin(PluginRespirator pr) {
 		Main.pr = pr;
 		library = Library.init(pr);
-		Search.setup(library);
+		Search.setup(library, pr.getNode().executor);
 		webinterface = new WebInterface(library, pr);
 		webinterface.load();
 	}

src/plugins/Library/search/Search.java
@@ -3,6 +3,8 @@
  * http://www.gnu.org/ for further details of the GPL. */
 package plugins.Library.search;
 
+import freenet.support.Executor;
+import freenet.support.HTMLNode;
 import plugins.Library.Library;
 import plugins.Library.index.Request;
 import plugins.Library.index.AbstractRequest;
@@ -23,6 +25,7 @@ import plugins.Library.index.TermEntry;
 import plugins.Library.search.ResultSet.ResultOperation;
 import plugins.Library.serial.CompositeProgress;
 import plugins.Library.serial.Progress;
+import plugins.Library.ui.ResultNodeGenerator;
 
 /**
  * Performs asynchronous searches over many index or with many terms and search logic
@@ -33,6 +36,7 @@ public class Search extends AbstractRequest<Set<TermEntry>>
 				implements CompositeRequest<Set<TermEntry>> {
 
 	private static Library library;
+	private static Executor executor;
 
 	private ResultOperation resultOperation;
 
@@ -41,13 +45,20 @@ public class Search extends AbstractRequest<Set<TermEntry>>
 	private String query;
 	private String indexURI;
 
-	protected int stage=0;
-	protected int stageCount;
-
 	private static HashMap<String, Search> allsearches = new HashMap<String, Search>();
 	private static HashMap<Integer,Search> searchhashes = new HashMap<Integer, Search>();
+	private ResultSet resultset;
 
-	private enum SearchStatus { Unstarted, Busy, Ready, Combining, Done };
+	/**
+	 * Settings for producing result nodes, if true a HTMLNode of the results will be generated after the results are complete which can be accessed via getResultNode()
+	 */
+	private boolean formatResult = false;
+	private boolean htmlgroupusk;
+	private boolean htmlshowold;
+	private boolean htmljs;
+	private ResultNodeGenerator resultNodeGenerator;
+
+	private enum SearchStatus { Unstarted, Busy, Combining, Formatting, Done };
 	private SearchStatus status = SearchStatus.Unstarted;
 
 
@@ -62,6 +73,10 @@ public class Search extends AbstractRequest<Set<TermEntry>>
 	 *
 	 * @param search string to be searched
 	 * @param indexuri URI of index(s) to be used
+	 * @param generateResultNode if set, the search operation will conclude with
+	 * generating the results for displaying, if not set this step will be skipped.
+	 * This action is not guarenteed, in the returned Search as this search may
+	 * have already been started.
 	 * @throws InvalidSearchException if any part of the search is invalid
 	 */
 	public static Search startSearch(String search, String indexuri) throws InvalidSearchException, TaskAbortException{
@@ -78,14 +93,16 @@ public class Search extends AbstractRequest<Set<TermEntry>>
 		String[] indices = indexuri.split("[ ;]");
 		if(indices.length<1 || search.trim().length()<1)
 			throw new InvalidSearchException("Attempt to start search with no index or terms");
-		else if(indices.length==1)
-			return splitQuery(search, indexuri);
-		else{
+		else if(indices.length==1){
+			Search newSearch = splitQuery(search, indexuri);
+			return newSearch;
+		}else{
 			// create search for multiple terms over multiple indices
 			ArrayList<Request> indexrequests = new ArrayList(indices.length);
 			for (String index : indices)
 				indexrequests.add(startSearch(search, index));
-			return new Search(search, indexuri, indexrequests, ResultOperation.DIFFERENTINDEXES);
+			Search newSearch = new Search(search, indexuri, indexrequests, ResultOperation.DIFFERENTINDEXES);
+			return newSearch;
 		}
 	}
 
@@ -125,7 +142,11 @@ public class Search extends AbstractRequest<Set<TermEntry>>
 		this.query = query;
 		this.indexURI = indexURI;
 		this.resultOperation = resultOperation;
-		this.status = SearchStatus.Busy;
+		try {
+			setStatus();
+		} catch (TaskAbortException ex) {
+			setError(ex);
+		}
 
 		storeSearch(this);
 		Logger.minor(this, "Created Search object for with subRequests :"+subsearches);
@@ -149,6 +170,11 @@ public class Search extends AbstractRequest<Set<TermEntry>>
 		this.query = query;
 		this.indexURI = indexURI;
 		this.resultOperation = ResultOperation.SINGLE;
+		try {
+			setStatus();
+		} catch (TaskAbortException ex) {
+			setError(ex);
+		}
 		storeSearch(this);
 	}
 
@@ -246,8 +272,9 @@ public class Search extends AbstractRequest<Set<TermEntry>>
 	/**
 	 * Sets the parent plugin to be used for logging & plugin api
 	 */
-	public static void setup(Library library){
+	public static void setup(Library library, Executor executor){
 		Search.library = library;
+		Search.executor = executor;
 		Search.allsearches = new HashMap<String, Search>();
 	}
 
@@ -305,11 +332,16 @@ public class Search extends AbstractRequest<Set<TermEntry>>
 		return search + "@" + indexuri;
 	}
 
-	@Override
 	/**
 	 * A descriptive string for logging
 	 */
+	@Override
 	public String toString(){
+		try {
+			setStatus();
+		} catch (TaskAbortException ex) {
+			setError(ex);
+		}
 		return "Search: "+resultOperation+" : "+subject+" : "+subsearches;
 	}
 
@@ -338,69 +370,95 @@ public class Search extends AbstractRequest<Set<TermEntry>>
 
 
 	/**
-	 * @return true if all are Finished, false otherwise
+	 * @return true if all are Finished and Result is ready, also stimulates the creation of the result if all subreqquests are complete and the result isn't made
 	 */
 	@Override public boolean isDone() throws TaskAbortException{
+		setStatus();
+		return status == SearchStatus.Done;
+	}
+
+	/**
+	 * Returns whether the generator has formatted the results
+	 * @return
+	 */
+	public boolean hasGeneratedResultNode(){
+		return resultNodeGenerator != null && resultNodeGenerator.isDone();
+	}
+
+	public HTMLNode getHTMLNode(){
+		return resultNodeGenerator.getPageEntryNode();
+	}
+	
+	/**
+	 * After this finishes running, the status of this Search object will be correct, stimulates the creation of the result if all subreqquests are complete and the result isn't made
+	 * @throws plugins.Library.serial.TaskAbortException
+	 */
+	private synchronized void setStatus() throws TaskAbortException{
+		switch (status){
+			case Unstarted :	// If Unstarted, status -> Busy
+				status = SearchStatus.Busy;
+			case Busy :
+				if(!isSubRequestsComplete())
+					return;		// If Busy & still waiting for subrequests to complete, status remains Busy
+				// If subrequests have completed start process to combine results
+				resultset = new ResultSet(subject, resultOperation, subsearches);
+				if(executor!=null)
+					executor.execute(resultset, "Library.Search : combining results");
+				else
+					(new Thread(resultset, "Library.Search : combining results")).start();
+				status = SearchStatus.Combining;
+			case Combining :
+				if(resultset!=null && !resultset.isDone())
+					return;		// If Combining & combine not finished, status remains as Combining
+				// If finished Combining and asked to generate resultnode, start that process
+				if(formatResult){
+					// resultset doesn't exist but subrequests are complete so we can start up a resultset
+					resultNodeGenerator = new ResultNodeGenerator(resultset, htmlgroupusk, htmlshowold, htmljs);
+					if(executor!=null)
+						executor.execute(resultset, "Library.Search : combining results");
+					else
+						(new Thread(resultset, "Library.Search : combining results")).start();
+					status = SearchStatus.Formatting;	// status -> Formatting
+				}else			// If not asked to format output, status -> done
+					status = SearchStatus.Done;
+			case Formatting :
+				// If asked to generate resultnode and still doing that, status remains as Formatting
+				if(formatResult && resultNodeGenerator !=null && !resultNodeGenerator.isDone())
+					return;
+				// If finished Formatting or not asked to do so, status -> Done
+				status = SearchStatus.Done;
+			case Done :
+				// Done , do nothing
+		}
+	}
+	
+	/**
+	 * @return true if all are Finished, false otherwise
+	 */
+	private boolean isSubRequestsComplete() throws TaskAbortException{
 		for(Request r : subsearches)
 			if(!r.isDone())
 				return false;
 		return true;
 	}
-
-//	/**
-//	 * @return sum of NumBlocksCompleted
-//	 */
-//	@Override public int partsDone() {
-////		if(progressAccessed())
-////			return blocksCompleted;
-//		blocksCompleted=0;
-//		for(Request r : subsearches)
-//			blocksCompleted+=r.partsDone();
-//		return blocksCompleted;
-//	}
-//
-//	/**
-//	 * @return sum of NumBlocksTotal
-//	 * TODO record all these progress things to speed up
-//	 */
-//	@Override public int partsTotal() {
-////		if(progressAccessed())
-////			return blocksTotal;
-//		blocksTotal=0;
-//		for(Request r : subsearches)
-//			blocksTotal+=r.partsTotal();
-//		return blocksTotal;
-//	}
-//
-//	/**
-//	 * @return true if all subsearches numblocks are final
-//	 */
-//	@Override public boolean isTotalFinal() {
-//		for(Request r : subsearches)
-//			if(!r.isTotalFinal())
-//				return false;
-//		return true;
-//	}
+	
 
 	/**
-	 * Use ResultSet to perform Set operations on subsearches <br />
-	 * @return Set of URIWrappers
+	 * Return the set of results or null if it is not ready <br />
+	 * @return Set of TermEntry
 	 */
 	@Override public Set<TermEntry> getResult() throws TaskAbortException {
 		if(!isDone())
 			return null;
 
-		status = SearchStatus.Combining;	// This wont show as this operation blocks
-		
-		allsearches.remove(getSubject());
-		searchhashes.remove(hashCode());
+		return resultset;
+	}
 
-		ResultSet result;
-		result = new ResultSet(subject, resultOperation, subsearches);
-
-		status = SearchStatus.Done;
-
-		return result;
+	public synchronized void setMakeResultNode(boolean groupusk, boolean showold, boolean js){
+		formatResult = true;
+		htmlgroupusk = groupusk;
+		htmlshowold = showold;
+		htmljs = js;
 	}
 
 	@Override
@@ -411,13 +469,11 @@ public class Search extends AbstractRequest<Set<TermEntry>>
 	@Override
 	public String getStatus() {
 		try {
-			if (status.compareTo(SearchStatus.Ready) < 0 && isDone()) {
-				status = SearchStatus.Ready;
-			}
+			setStatus();
+			return status.name();
 		} catch (TaskAbortException ex) {
-			return "Error";
+			return "Error finding Status";
 		}
-		return status.name();
 	}
 
 	public boolean isPartiallyDone() {

src/plugins/Library/ui/MainPage.java
@@ -216,15 +216,20 @@ class MainPage {
 				contentNode.addChild(progressBox());
 				// If search is complete show results
 				if (search.isDone()) {
-					// TODO dont do it this way
-					try {
-						ResultNodeGenerator nodegenerator = new ResultNodeGenerator(search.getResult(), groupusk);
-						contentNode.addChild(nodegenerator.generatePageEntryNode(showold, js));
-					} catch (TaskAbortException ex) {
-						exceptions.add(ex);
-					} catch (RuntimeException ex) {
-						exceptions.add(ex);
-					}
+					if(search.hasGeneratedResultNode()){
+						contentNode.addChild(search.getHTMLNode());
+						Logger.normal(this, "Got pre generated result node.");
+					}else
+						try {
+							Logger.normal(this, "Blocking to generate resultnode.");
+							ResultNodeGenerator nodegenerator = new ResultNodeGenerator(search.getResult(), groupusk, showold, js);
+							nodegenerator.run();
+							contentNode.addChild(nodegenerator.getPageEntryNode());
+						} catch (TaskAbortException ex) {
+							exceptions.add(ex);
+						} catch (RuntimeException ex) {
+							exceptions.add(ex);
+						}
 				} else {
 					contentNode.addChild("div", "id", "results").addChild("#");
 				}

src/plugins/Library/ui/ResultNodeGenerator.java
@@ -21,21 +21,53 @@ import plugins.Library.index.TermTermEntry;
 
 
 /**
- * Class for parsing and formatting search results
+ * Class for parsing and formatting search results, once isDone() return true, the nodes are ready to use
  *
  * @author MikeB
  */
-public class ResultNodeGenerator {
-	TreeMap<String, TermPageGroupEntry> groupmap;
-	TreeSet<TermPageEntry> pageset;
-	TreeSet<TermTermEntry> relatedTerms;
-	TreeSet<TermIndexEntry> relatedIndexes;
+public class ResultNodeGenerator implements Runnable {
+	private TreeMap<String, TermPageGroupEntry> groupmap;
+	private TreeSet<TermPageEntry> pageset;
+	private TreeSet<TermTermEntry> relatedTerms;
+	private TreeSet<TermIndexEntry> relatedIndexes;
+	private Set<TermEntry> result;
+	private boolean groupusk;
+	private boolean showold;
+	private boolean js;
+	private boolean done;
+	private HTMLNode pageEntryNode;
 
+
+
+	public ResultNodeGenerator(Set<TermEntry> result, boolean groupusk, boolean showold, boolean js){
+		this.result = result;
+		this.groupusk = groupusk;
+		this.showold = showold;
+		this.js = js;
+	}
+
+
+	public synchronized void run(){
+		if(done)
+			throw new IllegalStateException("ResultNodeGenerator can only be run once.");
+
+		parseResult();
+		generatePageEntryNode();
+		done = true;
+	}
+
+	public HTMLNode getPageEntryNode(){
+		return pageEntryNode;
+	}
+
+	public boolean isDone(){
+		return done;
+	}
 
 	/**
 	 * Parse result into generator
 	 */
-	public ResultNodeGenerator(Set<TermEntry> result, boolean groupusk){
+	private void parseResult(){
 		if(groupusk)
 			groupmap = new TreeMap();
 		else
@@ -83,19 +115,19 @@ public class ResultNodeGenerator {
 		}
     }
 	
-	public HTMLNode generateIndexEntryNode(){
+	private HTMLNode generateIndexEntryNode(){
 		return new HTMLNode("#", "TermIndexEntry code not done yet");
 	}
 
-	public HTMLNode generateTermEntryNode(){
+	private HTMLNode generateTermEntryNode(){
 		return new HTMLNode("#", "TermTermEntry code not done yet");
 	}
 
 	/**
 	 * Generate node of page results from this generator
 	 */
-	public HTMLNode generatePageEntryNode(boolean showold, boolean js){
-		HTMLNode pageListNode = new HTMLNode("div", "id", "results");
+	private void generatePageEntryNode(){
+		pageEntryNode = new HTMLNode("div", "id", "results");
 
 		int results = 0;
 		// Loop to separate results into SSK groups
@@ -110,7 +142,7 @@ public class ResultNodeGenerator {
 				TermPageGroupEntry group = it2.next();
 				String keybase = group.getSubject();
 				SortedMap<Long, SortedSet<TermPageEntry>> siteMap = group.getEditions();
-				HTMLNode siteNode = pageListNode.addChild("div", "style", "padding-bottom: 6px;");
+				HTMLNode siteNode = pageEntryNode.addChild("div", "style", "padding-bottom: 6px;");
 				// Create a block for old versions of this SSK
 				HTMLNode siteBlockOldOuter = siteNode.addChild("div", new String[]{"id", "style"}, new String[]{"result-hiddenblock-"+keybase, (!showold?"display:none":"")});
 				// put title on block if it has more than one version in it
@@ -154,12 +186,11 @@ public class ResultNodeGenerator {
 		}else{
 			for (Iterator<TermPageEntry> it = pageset.iterator(); it.hasNext();) {
 				TermPageEntry termPageEntry = it.next();
-				pageListNode.addChild("div").addChild(termPageEntryNode(termPageEntry, true));
+				pageEntryNode.addChild("div").addChild(termPageEntryNode(termPageEntry, true));
 				results++;
 			}
 		}
-		pageListNode.addChild("p").addChild("span", "class", "librarian-summary-found", "Found "+results+" results");
-		return pageListNode;
+		pageEntryNode.addChild("p").addChild("span", "class", "librarian-summary-found", "Found "+results+" results");
 	}
 	
 	private HTMLNode termPageEntryNode(TermPageEntry entry,boolean newestVersion) {

commit 7b3ca325b5605d467b4c49026a6d5767d1a4cd46

Fixed stupid mistake preventing search working introduced in 40763ffe9e5dc295210aeb9595dee648db002c07

src/plugins/Library/search/ResultSet.java
@@ -97,6 +97,7 @@ public class ResultSet implements Set<TermEntry>, Runnable{
 				phrase(subresults);
 				break;
 		}
+		done = true;
 	}
 
 	/**