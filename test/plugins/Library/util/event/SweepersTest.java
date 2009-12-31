4a66fec8bbf36e36fd2f02e031ab6108f87d1326
Ximin Luo <xl269@cam.ac.uk>
2009-12-31 00:22:07 +0000

    - use new name in test too, whoops

--- test/plugins/Library/util/event/SweepersTest.java
+++ test/plugins/Library/util/event/SweepersTest.java
@@ -45,112 +45,112 @@ public class SweepersTest extends TestCase {
 
 	@SuppressWarnings("unchecked")
 	public void testAfterNew(final Sweeper sw) {
-		assertTrue(sw.getState() == Sweeper.SweeperState.NEW);
+		assertTrue(sw.getState() == Sweeper.State.NEW);
 		testIllegalStateException(new Runnable() { public void run() { sw.acquire(testobj[0]); } }, "acquiring when not open");
 		testIllegalStateException(new Runnable() { public void run() { sw.acquire(testobj[1]); } }, "acquiring when not open");
 		testIllegalStateException(new Runnable() { public void run() { sw.acquire(testobj[2]); } }, "acquiring when not open");
 		testIllegalStateException(new Runnable() { public void run() { sw.release(testobj[0]); } }, "releasing when not open");
 		testIllegalStateException(new Runnable() { public void run() { sw.close(); } }, "closing when not open");
 		assertTrue(sw.size() == 0);
-		assertTrue(sw.getState() == Sweeper.SweeperState.NEW);
+		assertTrue(sw.getState() == Sweeper.State.NEW);
 		sw.open();
 		testIllegalStateException(new Runnable() { public void run() { sw.open(); } }, "opening twice");
-		assertTrue(sw.getState() == Sweeper.SweeperState.OPEN);
+		assertTrue(sw.getState() == Sweeper.State.OPEN);
 	}
 
 	@SuppressWarnings("unchecked")
 	public void testCloseClear(final Sweeper sw) {
-		assertTrue(sw.getState() == Sweeper.SweeperState.OPEN);
+		assertTrue(sw.getState() == Sweeper.State.OPEN);
 		sw.acquire(testobj[0]);
 		sw.acquire(testobj[1]);
 		sw.acquire(testobj[2]);
 		sw.release(testobj[0]);
 		sw.release(testobj[1]);
 		sw.release(testobj[2]);
-		assertTrue(sw.getState() == Sweeper.SweeperState.OPEN);
+		assertTrue(sw.getState() == Sweeper.State.OPEN);
 		sw.acquire(testobj[0]);
 		sw.acquire(testobj[1]);
 		sw.acquire(testobj[2]);
 		sw.release(testobj[0]);
 		sw.release(testobj[1]);
 		sw.release(testobj[2]);
-		assertTrue(sw.getState() == Sweeper.SweeperState.OPEN);
+		assertTrue(sw.getState() == Sweeper.State.OPEN);
 		sw.close();
-		assertTrue(sw.getState() == Sweeper.SweeperState.CLEARED);
+		assertTrue(sw.getState() == Sweeper.State.CLEARED);
 	}
 
 	@SuppressWarnings("unchecked")
 	public <S extends Sweeper & Iterable> void testCloseClearIterable(final S sw) {
-		assertTrue(sw.getState() == Sweeper.SweeperState.OPEN);
+		assertTrue(sw.getState() == Sweeper.State.OPEN);
 		sw.acquire(testobj[0]);
 		sw.acquire(testobj[1]);
 		sw.acquire(testobj[2]);
 		for (Iterator it = sw.iterator(); it.hasNext();) { it.next(); it.remove(); }
-		assertTrue(sw.getState() == Sweeper.SweeperState.OPEN);
+		assertTrue(sw.getState() == Sweeper.State.OPEN);
 		sw.acquire(testobj[0]);
 		sw.acquire(testobj[1]);
 		sw.acquire(testobj[2]);
 		for (Iterator it = sw.iterator(); it.hasNext();) { it.next(); it.remove(); }
-		assertTrue(sw.getState() == Sweeper.SweeperState.OPEN);
+		assertTrue(sw.getState() == Sweeper.State.OPEN);
 		sw.close();
-		assertTrue(sw.getState() == Sweeper.SweeperState.CLEARED);
+		assertTrue(sw.getState() == Sweeper.State.CLEARED);
 	}
 
 	@SuppressWarnings("unchecked")
 	public void testReleaseClear(final Sweeper sw) {
-		assertTrue(sw.getState() == Sweeper.SweeperState.OPEN);
+		assertTrue(sw.getState() == Sweeper.State.OPEN);
 		sw.acquire(testobj[0]);
 		sw.acquire(testobj[1]);
 		sw.acquire(testobj[2]);
 		sw.release(testobj[0]);
 		sw.release(testobj[1]);
 		sw.release(testobj[2]);
-		assertTrue(sw.getState() == Sweeper.SweeperState.OPEN);
+		assertTrue(sw.getState() == Sweeper.State.OPEN);
 		sw.acquire(testobj[0]);
 		sw.acquire(testobj[1]);
 		sw.acquire(testobj[2]);
-		assertTrue(sw.getState() == Sweeper.SweeperState.OPEN);
+		assertTrue(sw.getState() == Sweeper.State.OPEN);
 		sw.close();
-		assertTrue(sw.getState() == Sweeper.SweeperState.CLOSED);
+		assertTrue(sw.getState() == Sweeper.State.CLOSED);
 		testAfterClose(sw);
 		sw.release(testobj[0]);
 		sw.release(testobj[1]);
 		sw.release(testobj[2]);
-		assertTrue(sw.getState() == Sweeper.SweeperState.CLEARED);
+		assertTrue(sw.getState() == Sweeper.State.CLEARED);
 	}
 
 	@SuppressWarnings("unchecked")
 	public <S extends Sweeper & Iterable> void testReleaseClearIterable(final S sw) {
-		assertTrue(sw.getState() == Sweeper.SweeperState.OPEN);
+		assertTrue(sw.getState() == Sweeper.State.OPEN);
 		sw.acquire(testobj[0]);
 		sw.acquire(testobj[1]);
 		sw.acquire(testobj[2]);
 		for (Iterator it = sw.iterator(); it.hasNext();) { it.next(); it.remove(); }
-		assertTrue(sw.getState() == Sweeper.SweeperState.OPEN);
+		assertTrue(sw.getState() == Sweeper.State.OPEN);
 		sw.acquire(testobj[0]);
 		sw.acquire(testobj[1]);
 		sw.acquire(testobj[2]);
 		sw.close();
-		assertTrue(sw.getState() == Sweeper.SweeperState.CLOSED);
+		assertTrue(sw.getState() == Sweeper.State.CLOSED);
 		testAfterClose(sw);
 		for (Iterator it = sw.iterator(); it.hasNext();) { it.next(); it.remove(); }
-		assertTrue(sw.getState() == Sweeper.SweeperState.CLEARED);
+		assertTrue(sw.getState() == Sweeper.State.CLEARED);
 	}
 
 	@SuppressWarnings("unchecked")
 	public void testAfterClose(final Sweeper sw) {
-		assertTrue(sw.getState() == Sweeper.SweeperState.CLOSED);
+		assertTrue(sw.getState() == Sweeper.State.CLOSED);
 		int s = sw.size();
 		assertTrue(s > 0);
 		testIllegalStateException(new Runnable() { public void run() { sw.open(); } }, "opening when closed");
 		testIllegalStateException(new Runnable() { public void run() { sw.acquire(testobj[0]); } }, "acquiring when closed");
-		assertTrue(sw.getState() == Sweeper.SweeperState.CLOSED);
+		assertTrue(sw.getState() == Sweeper.State.CLOSED);
 		assertTrue(sw.size() == s);
 	}
 
 	@SuppressWarnings("unchecked")
 	public void testAfterClear(final Sweeper sw) {
-		assertTrue(sw.getState() == Sweeper.SweeperState.CLEARED);
+		assertTrue(sw.getState() == Sweeper.State.CLEARED);
 		testIllegalStateException(new Runnable() { public void run() { sw.open(); } }, "opening when cleared");
 		testIllegalStateException(new Runnable() { public void run() { sw.acquire(testobj[0]); } }, "acquiring when cleared");
 		testIllegalStateException(new Runnable() { public void run() { sw.acquire(testobj[1]); } }, "acquiring when cleared");
@@ -158,7 +158,7 @@ public class SweepersTest extends TestCase {
 		testIllegalStateException(new Runnable() { public void run() { sw.release(testobj[0]); } }, "releasing when cleared");
 		testIllegalStateException(new Runnable() { public void run() { sw.close(); } }, "closing when cleared");
 		assertTrue(sw.size() == 0);
-		assertTrue(sw.getState() == Sweeper.SweeperState.CLEARED);
+		assertTrue(sw.getState() == Sweeper.State.CLEARED);
 	}
 
 	public void testIllegalStateException(Runnable r, String s) {
