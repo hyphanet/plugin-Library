53613943c7aec8a461deac9425651553c11ceff7
Matthew Toseland <toad@amphibian.dyndns.org>
2010-06-11 23:44:16 +0100

    Version 11.

--- src/plugins/Library/Library.java
+++ src/plugins/Library/Library.java
@@ -83,7 +83,7 @@ final public class Library implements URLUpdateHook {
 
 	public static final String BOOKMARK_PREFIX = "bookmark:";
 	public static final String DEFAULT_INDEX_SITE = BOOKMARK_PREFIX + "wanna";
-	private static int version = 10;
+	private static int version = 11;
 	public static final String plugName = "Library " + getVersion();
 
 	public static String getPlugName() {
