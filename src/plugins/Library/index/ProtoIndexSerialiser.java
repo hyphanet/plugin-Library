9993f54ce89bc6424e2249bafc80a6f8b3b10cf6
Matthew Toseland <toad@amphibian.dyndns.org>
2013-08-01 15:12:59 +0100

    This is always a LiveArchiver (lets us get rid of some dodgy type checks too, and makes the next commit a lot easier)

--- src/plugins/Library/index/ProtoIndexSerialiser.java
+++ src/plugins/Library/index/ProtoIndexSerialiser.java
@@ -7,6 +7,7 @@ import plugins.Library.Library;
 import plugins.Library.client.FreenetArchiver;
 import plugins.Library.util.SkeletonBTreeMap;
 import plugins.Library.util.SkeletonBTreeSet;
+import plugins.Library.util.exec.SimpleProgress;
 import plugins.Library.util.exec.TaskAbortException;
 import plugins.Library.io.serial.Serialiser.*;
 import plugins.Library.io.serial.LiveArchiver;
@@ -50,10 +51,10 @@ implements Archiver<ProtoIndex>,
 	final protected Translator<ProtoIndex, Map<String, Object>>
 	trans;
 
-	final protected Archiver<Map<String, Object>>
+	final protected LiveArchiver<Map<String, Object>, SimpleProgress> 
 	subsrl;
 
-	public ProtoIndexSerialiser(Archiver<Map<String, Object>> s) {
+	public ProtoIndexSerialiser(LiveArchiver<Map<String, Object>, SimpleProgress> s) {
 		subsrl = s;
 		trans = new IndexTranslator(subsrl);
 	}
@@ -148,9 +149,9 @@ implements Archiver<ProtoIndex>,
 		SkeletonBTreeMap.TreeTranslator<URIKey, SkeletonBTreeMap<FreenetURI, URIEntry>>(null, new
 		ProtoIndexComponentSerialiser.TreeMapTranslator<URIKey, SkeletonBTreeMap<FreenetURI, URIEntry>>(null));
 
-		private Archiver<Map<String, Object>> subsrl;
+		private LiveArchiver<Map<String, Object>, SimpleProgress> subsrl;
 		
-		public IndexTranslator(Archiver<Map<String, Object>> subsrl) {
+		public IndexTranslator(LiveArchiver<Map<String, Object>, SimpleProgress> subsrl) {
 			this.subsrl = subsrl;
 		}
 
@@ -191,7 +192,7 @@ implements Archiver<ProtoIndex>,
 			if (magic == ProtoIndex.serialVersionUID) {
 				try {
 					// FIXME yet more hacks related to the lack of proper asynchronous FreenetArchiver...
-					ProtoIndexComponentSerialiser cmpsrl = ProtoIndexComponentSerialiser.get((Integer)map.get("serialFormatUID"), subsrl instanceof LiveArchiver ? ((LiveArchiver)subsrl) : null);
+					ProtoIndexComponentSerialiser cmpsrl = ProtoIndexComponentSerialiser.get((Integer)map.get("serialFormatUID"), subsrl);
 					FreenetURI reqID = (FreenetURI)map.get("reqID");
 					String name = (String)map.get("name");
 					String ownerName = (String)map.get("ownerName");
