575095c35e549572c7624f7e120b85ceb43170f2
Debora Wöpcke <deboradub@gmail.com>
2015-07-12 08:06:54 +0200

    Script to run the upload perpetually.

---
+++ loop.sh
@@ -0,0 +1,8 @@
+#!/bin/sh -ex
+
+while test -f library.continue.loop
+do
+    # tail wrapper.log
+    # ls -ltr library.index.*
+    java -jar ../projects/freenet/github/plugin-Library/dist/uploader.jar
+done
